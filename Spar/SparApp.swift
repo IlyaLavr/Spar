78eb6c0d662bb94e2ed05a4b1b5b925004ea0ad6
Ilya Lavrinov <iilavr@mail.ru>
2023-08-13 00:27:41 +0400

    Initial Commit

---
+++ Spar/SparApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparApp.swift
+//  Spar
+//
+//  Created by Илья on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
